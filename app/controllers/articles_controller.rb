31808e7a7d5f582ea62067d270f1601e9bc8375b
oloomoses <olooine@gmail.com>
2020-07-28 01:36:09 +0300

    update functionality added to the app

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,4 +25,17 @@ class ArticlesController < ApplicationController
     redirect_to articles_path
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    flash.notice = "Article '#{@article.title}' Updated!"
+
+    redirect_to article_path(@article)
+  end
+
 end
