f468e078beaa6257c1484541772e9c29e3f117bd
eduardo <eduardoreisalvarenga@gmail.com>
2020-07-28 13:37:37 -0300

    Added comments section in our blog page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
+
   root to: 'articles#index'
 
 end
