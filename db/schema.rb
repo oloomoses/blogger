7c864baab1f88495d0577a1ea0e2f1f3770f1228
eduardo <eduardoreisalvarenga@gmail.com>
2020-07-29 13:38:52 -0300

    Added paperclip and magick to project

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_28_172431) do
+ActiveRecord::Schema.define(version: 2020_07_29_154535) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.integer "image_update_at"
   end
 
   create_table "comments", force: :cascade do |t|
